7f260d984b4e045d461a4875db39798ba3de0536
sleepiie <122366817+sleepiie@users.noreply.github.com>
2024-09-29 21:00:39 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico', // Path to your favicon
+        },
+      ],
+    },
+  },
 })
